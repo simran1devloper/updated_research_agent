2026249ec6bdceaeb1f13f59db7187c2e41c7684
Simran <158156836+simran1devloper@users.noreply.github.com>
2026-03-06 00:10:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 # Load environment variables from a .env file
 load_dotenv()
-OLLAMA_URL = "https://f227-103-72-8-227.ngrok-free.app/api/generate"
+OLLAMA_URL = "https://f227-103-72-8-227.ngrok-free.app"
 MODEL = "gemma3"
 
 class Config:
     # --- Model Configuration ---
     MODEL_NAME = "ministral-3:3b-cloud"  # Local Ollama model
     TEMPERATURE = 0
-    OLLAMA_BASE_URL = "https://f227-103-72-8-227.ngrok-free.app/api/generate"
+    OLLAMA_BASE_URL = "https://f227-103-72-8-227.ngrok-free.app"
     
     # --- API Keys ---
     # Ensure these are set in your .env file
